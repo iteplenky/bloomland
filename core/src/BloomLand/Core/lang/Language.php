7d574cc495385570c969969595485ead45d6a77d
Denis Klyuyev <42160062+iteplenky@users.noreply.github.com>
2021-06-26 18:36:43 +0600

    code reformated

--- core/src/BloomLand/Core/lang/Language.php
+++ core/src/BloomLand/Core/lang/Language.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 
 namespace BloomLand\Core\lang;
 
@@ -14,79 +12,82 @@ namespace BloomLand\Core\lang;
 
         protected static $lang = [];
 
-        public static function init(): void
+        public static function init() : void
         {
             self::$lang[self::DEFAULT_LANGUAGE] = self::loadLang(__DIR__ . '/locale/' . self::DEFAULT_LANGUAGE . '.ini');
 
             // LOAD LANGS
             foreach (glob(__DIR__ . '/locale/*.ini') as $locale) {
+
                 $localeName = substr(basename($locale), 0, -4);
-                if ($localeName == self::DEFAULT_LANGUAGE) {
-                    continue;
-                }
+                if ($localeName == self::DEFAULT_LANGUAGE) continue;
 
                 $loadLang = self::loadLang($locale);
+
                 if (empty($loadLang)) continue;
+                
                 self::$lang[$localeName] = array_merge(self::$lang[self::DEFAULT_LANGUAGE], $loadLang);
+            
             }
 
             Core::getAPI()->getLogger()->info('LanguageManager > §b'  . count(self::$lang) . '§r языков загружено!');
         }
 
-        public static function getLanguages(): array 
+        public static function getLanguages() : array 
         {
             return array_keys(self::$lang);
         }
 
-        public static function getTranslatedLanguageNames(): array 
+        public static function getTranslatedLanguageNames() : array 
         {
-            return array_map(function(string $locale): string {
+            return array_map(function(string $locale) : string {
                 return Language::translate($locale, 'language.name');
             }, self::getLanguages());
         }
 
-        public static function loadLang(string $path): array 
+        public static function loadLang(string $path) : array 
         {
-            return file_exists($path) ? array_map('stripcslashes', parse_ini_file($path, false, INI_SCANNER_RAW)): [];
+            return file_exists($path) ? array_map('stripcslashes', parse_ini_file($path, false, INI_SCANNER_RAW)) : [];
         }
 
-        public static function mergeLanguage(string $locale, array $langFile): void 
+        public static function mergeLanguage(string $locale, array $langFile) : void 
         {
             self::$lang[$locale] = array_merge((self::$lang[$locale] ?? []), $langFile);
         }
 
-        public static function translate(string $locale, string $text, array $args = []): string 
+        public static function translate(string $locale, string $text, array $args = []) : string 
         {
             $translatedText = self::$lang[$locale][$text] ?? null;
-            if ($translatedText === null) {
-                return $text;
-            } elseif (!empty($args)) {
-                $translatedText = sprintf ($translatedText, ...$args);
-            }
+            
+            if ($translatedText === null) return $text;
+
+            elseif (!empty($args)) $translatedText = sprintf ($translatedText, ...$args);
+            
             return $translatedText;
         }
 
-        public static function translateExtended(string $locale, string $text, array $args = [], string $separator = '%'): string 
+        public static function translateExtended(string $locale, string $text, array $args = [], string $separator = '%') : string 
         {
             $isEmpty = empty($args);
-            if (!$isEmpty) {
-                array_unshift($args, -1); 
-            }
+            
+            if (!$isEmpty) array_unshift($args, -1);
+
             return preg_replace_callback('/' . $separator . '[a-zA-Z._]+/mi', function(array $str) use($locale, $isEmpty, $args) {
                 $str = $str[0];
                 return self::translate($locale, substr($str, 1), $isEmpty ? []: next($args));
             }, $text);
         }
 
-        public static function translateOrExtended(string $locale, string $text, array $parameters = [], string $separator = '%'): string 
+        public static function translateOrExtended(string $locale, string $text, array $parameters = [], string $separator = '%') : string 
         {
-            return isset(self::$lang[$locale][$text]) ? self::translate($locale, $text, $parameters): self::translateExtended($locale, $text, $parameters, $separator);
+            return isset(self::$lang[$locale][$text]) ? self::translate($locale, $text, $parameters) : self::translateExtended($locale, $text, $parameters, $separator);
         }
 
-        public static function getLang(string $locale): string 
+        public static function getLang(string $locale) : string 
         {
             return isset(self::$lang[$locale]) ? $locale: self::DEFAULT_LANGUAGE;
         }
+
     }
 
 ?>
