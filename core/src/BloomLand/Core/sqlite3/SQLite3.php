681ab0db452d5eec65409ea6aeb6256f92f87668
Denis Klyuyev <42160062+iteplenky@users.noreply.github.com>
2021-06-26 18:40:53 +0600

    questionable

--- core/src/BloomLand/Core/sqlite3/SQLite3.php
+++ core/src/BloomLand/Core/sqlite3/SQLite3.php
@@ -28,40 +28,4 @@ namespace BloomLand\Core\sqlite3;
             $prepare->execute();
         }
 
-        public static function addIntruder($username, $sender, $reason): void 
-        {
-            Core::getDatabase()->query("INSERT INTO `intruders` (`username`, `sender`, `reason`) VALUES ('$username', '$sender', '$reason')");
-        }
-
-        public static function removeIntruder($username) : void 
-        {
-            Core::getDatabase()->query("DELETE FROM `intruders` WHERE `username` = '{$username}'");
-        }
-
-        public static function getStringValue($username, string $key): string
-        {
-            $prepare = Core::getDatabase()->prepare("SELECT * FROM `data` WHERE username = :username");
-            $prepare->bindValue('username', $username);
-
-            $resource = $prepare->execute()->fetchArray(1);
-
-            if (!is_bool($resource)) {
-                return (string) $resource[$key];
-            }
-            return 'ru_RU';
-        }
-
-        public static function getIntValue($username, string $key): int
-        {
-            $prepare = Core::getDatabase()->prepare("SELECT * FROM `data` WHERE username = :username");
-            $prepare->bindValue('username', $username);
-
-            $resource = $prepare->execute()->fetchArray(1);
-
-            if (!is_bool($resource)) {
-                return (int) $resource[$key];
-            }
-            return 0;
-        }
-
     }
