9bf37342df2dc66a1b44f1e9be001f8a6062f29a
Denis Klyuyev <42160062+iteplenky@users.noreply.github.com>
2021-06-27 19:01:12 +0600

    refactored

--- core/src/BloomLand/Core/commands/staff/FlyCommand.php
+++ core/src/BloomLand/Core/commands/staff/FlyCommand.php
@@ -16,9 +16,16 @@ namespace BloomLand\Core\commands\staff;
             parent::__construct('fly', 'Управление режимом полета', '/fly');
         }
 
+        public function getPlugin() : Core
+        {
+            return Core::getAPI();
+        }
+
         public function execute(CommandSender $player, string $label, array $args) : bool
         {
-            if (Core::getAPI()->isEnabled()) {
+            if ($this->getPlugin()->isEnabled()) {
+
+                $prefix = $this->getPlugin()->getPrefix(); 
 
                 if (!$player->isCreative()) {
 
@@ -27,24 +34,25 @@ namespace BloomLand\Core\commands\staff;
                         $player->setAllowFlight(false);
                         $player->setFlying(false);
     
-                        $player->sendMessage(' §r> Вы §cвыключили§r режим полета.');
+                        $player->sendMessage($prefix . 'Вы §cвыключили§r режим полета.');
     
                     } else {
 
                         $player->setAllowFlight(true);
                         $player->setFlying(true);
     
-                        $player->sendMessage(' §r> Вы §bвключили§r режим полета.');
+                        $player->sendMessage($prefix . 'Вы §bвключили§r режим полета.');
                         
                     }
     
                 } else
-                    $player->sendMessage(' §r> Вы §cне можете §rуправлять режимом полета во время§b Творческого §rрежима.');
+                    $player->sendMessage($prefix . 'Вы §cне можете §rуправлять режимом полета во время§b Творческого §rрежима.');
 
             }
 
             return true;
         }
+        
     }
 
 ?>
