8a147c69566a0a40709783621c974a0ea473dd25
Denis Klyuyev <42160062+iteplenky@users.noreply.github.com>
2021-06-27 12:23:32 +0600

    SQLite3 function removed

--- core/src/BloomLand/Core/listener/ChatGameListener.php
+++ core/src/BloomLand/Core/listener/ChatGameListener.php
@@ -7,8 +7,6 @@ namespace BloomLand\Core\listener;
     use BloomLand\Core\Core;
     use BloomLand\Core\BLPlayer;
 
-    use BloomLand\Core\sqlite3\SQLite3;
-
     use pocketmine\event\Listener;
 
     use pocketmine\event\player\PlayerChatEvent;
@@ -26,8 +24,8 @@ namespace BloomLand\Core\listener;
             $message = $event->getMessage();
 
             if ($message == Core::getAPI()->chatGame) {
-
-                SQLite3::updateValue($player->getLowerCaseName(), 'coins', $player->getMoney() + 100 * mt_rand(1, 2));
+                
+                $player->addMoney(100 * mt_rand(1, 2));
 
                 $player->sendTitle('', '§fбонус начислен', 10, 20, 5);
                 
