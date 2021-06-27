f6b19991f98464b6f613a4b25a1b907f7b220659
Denis Klyuyev <42160062+iteplenky@users.noreply.github.com>
2021-06-27 18:53:33 +0600

    tabs fix

--- core/src/BloomLand/Core/commands/staff/BanCommand.php
+++ core/src/BloomLand/Core/commands/staff/BanCommand.php
@@ -33,15 +33,18 @@ namespace BloomLand\Core\commands\staff;
                         $intruder = Core::getAPI()->getServer()->getPlayerByPrefix($name);
 
                         if (empty($args[0])) {
-                            $player->sendMessage(Core::getAPI()->getPrefix() . '§eГерой§r, Вы заблокировали §b' . $intruder->getName() . '§r, не указав причину, пожалуйста осведомите его о причине.');
+                            $player->sendMessage(Core::getAPI()->getPrefix() . '§eГерой§r, Вы заблокировали §b' . $intruder->getName() . 
+                            '§r, не указав причину, пожалуйста осведомите его о причине.');
                             $args[0] = 'неизвестна';
                         }
 
                         Ban::add($intruder->getLowerCaseName(), $player->getLowerCaseName(), implode(" ", $args));
                         
-                        $intruder->kick('§l| §rВы заблокированы игроком §a' . $player->getName() . '§r.' . PHP_EOL . '§l| §rПричина: §e' . implode(" ", $args) . PHP_EOL . PHP_EOL . '§l| §rОставить жалобу: §bvk.com/bl_pe');
+                        $intruder->kick('§l| §rВы заблокированы игроком §a' . $player->getName() . '§r.' . PHP_EOL . '§l| §rПричина: §e' . 
+                        implode(" ", $args) . PHP_EOL . PHP_EOL . '§l| §rОставить жалобу: §bvk.com/bl_pe');
                         
-                        Core::getAPI()->getServer()->broadcastMessage(Core::getAPI()->getPrefix() . 'Игрок §b' . $intruder->getName() . ' §rзаблокирован от руки §d' . $player->getName() . '§r, по причине: §e' . implode(" ", $args) . '§r.');
+                        Core::getAPI()->getServer()->broadcastMessage(Core::getAPI()->getPrefix() . 'Игрок §b' . $intruder->getName() . 
+                       ' §rзаблокирован от руки §d' . $player->getName() . '§r, по причине: §e' . implode(" ", $args) . '§r.');
 
                     } else {
 
@@ -49,19 +52,22 @@ namespace BloomLand\Core\commands\staff;
 
                         if (Ban::isBanned(strtolower($intruder))) {
 
-                            $player->sendMessage(Core::getAPI()->getPrefix() . 'Игрок §b' . $intruder . ' §rуже ранее был заблокирован, по причине: §e' . Ban::get(strtolower($intruder), 'reason') . '§r.');
+                            $player->sendMessage(Core::getAPI()->getPrefix() . 'Игрок §b' . $intruder . 
+                            ' §rуже ранее был заблокирован, по причине: §e' . Ban::get(strtolower($intruder), 'reason') . '§r.');
                             return false;
 
                         }
 
                         if (empty($args[0])) {
-                            $player->sendMessage(Core::getAPI()->getPrefix() . '§eГерой§r, Вы заблокировали §b' . $intruder . '§r, не указав причину, пожалуйста осведомите его о причине.');
+                            $player->sendMessage(Core::getAPI()->getPrefix() . '§eГерой§r, Вы заблокировали §b' . $intruder . 
+                            '§r, не указав причину, пожалуйста осведомите его о причине.');
                             $args[0] = 'неизвестна';
                         }
 
                         Ban::add(strtolower($intruder), $player->getLowerCaseName(), implode(" ", $args));
 
-                        Core::getAPI()->getServer()->broadcastMessage(Core::getAPI()->getPrefix() . 'Игрок §b' . $intruder . ' §rзаблокирован от руки §d' . $player->getName() . '§r, по причине: §e' . implode(" ", $args) . '§r.');
+                        Core::getAPI()->getServer()->broadcastMessage(Core::getAPI()->getPrefix() . 'Игрок §b' . $intruder . 
+                        ' §rзаблокирован от руки §d' . $player->getName() . '§r, по причине: §e' . implode(" ", $args) . '§r.');
                     }
 
                 } else 
