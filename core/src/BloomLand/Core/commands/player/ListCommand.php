764eb9fcc3972f57b01006b995cecda26809a529
Denis Klyuyev <42160062+iteplenky@users.noreply.github.com>
2021-06-27 11:22:52 +0600

    useless code removed

--- core/src/BloomLand/Core/commands/player/ListCommand.php
+++ core/src/BloomLand/Core/commands/player/ListCommand.php
@@ -9,10 +9,6 @@ namespace BloomLand\Core\commands\player;
 
     use pocketmine\command\Command;
     use pocketmine\command\CommandSender;
-
-    use pocketmine\item\ItemFactory;
-
-    use BloomLand\Core\inventory\InventoryMenu;
     
     class ListCommand extends Command
     {
@@ -25,29 +21,8 @@ namespace BloomLand\Core\commands\player;
         {
             if (Core::getAPI()->isEnabled()) {
 
-                // if ($player instanceof BLPlayer) {
-
-                    // $playerNames = Core::getAPI()->getServer()->getOnlinePlayers();
-                    
-                    // sort($playerNames, SORT_STRING);
-
-                    $inv = new InventoryMenu(InventoryMenu::INVENTORY_TYPE_HOPPER);
-                    $inv->setName("§bInventory");
-                    $items =
-                        [
-                            0 => ItemFactory::getInstance()->get(1, 0, 1)->setCustomName("§r§e§lMarket"),
-                        ];
-    
-                    $inv->setItem($items);
-                    $inv->send($player);
-    
-                    if (CustomInventory::isOpeningInventoryMenu($player)) $player->sendMessage('open');
-                    else $player->sendMessage('not open');
-            
-                    $player->sendMessage(Core::getAPI()->getPrefix() . 'Сейчас играет §b' . count($player->getServer()->getOnlinePlayers()) . ' §rиз §3' . $player->getServer()->getMaxPlayers() . '§r.');
-                    // $player->sendMessage(Core::getAPI()->getPrefix() . implode(', ', $playerNames->getName()));
-
-                // }
+                $player->sendMessage(Core::getAPI()->getPrefix() . 'Сейчас играет §b' . count($player->getServer()->getOnlinePlayers()) . 
+                ' §rиз §3' . $player->getServer()->getMaxPlayers() . '§r.');
 
             }
             return true;
