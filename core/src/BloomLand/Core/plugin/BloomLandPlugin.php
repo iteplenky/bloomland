0eb45e49a94a40069e494308434c4e44d7bd3e3d
Denis Klyuyev <42160062+iteplenky@users.noreply.github.com>
2021-06-27 12:12:28 +0600

    useless code removed

--- core/src/BloomLand/Core/plugin/BloomLandPlugin.php
+++ core/src/BloomLand/Core/plugin/BloomLandPlugin.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 
 namespace BloomLand\Core\plugin;
 
@@ -12,21 +10,11 @@ namespace BloomLand\Core\plugin;
 
 	abstract class BloomLandPlugin extends PluginBase 
 	{
-		public const TABLE_PLAYERS = 'players';
-		public const TABLE_SKYBLOCK_MISSIONS = 'skyblockMissions';
-		public const TABLE_SKYBLOCK_PLAYERS = 'skyblockPlayers';
-		public const TABLE_CRIMINAL_RECORDS = 'criminals';
-		public const TABLE_PROMOTIONS = 'promotions';
-
 		public function onEnable() : void
 		{
 			Core::getAPI()->getLogger()->info('§e' . $this->getDescription()->getName() . " загружается...");
 		}
-
-		public function getResourcesDir() : string
-		{
-			return $this->getFile() . 'resources' . DIRECTORY_SEPARATOR;
-		}
+		
 	}
 
 ?>
