efe4e43ea0153f2bde73db7c567f47416fff5cdd
Denis Klyuyev <42160062+iteplenky@users.noreply.github.com>
2021-06-27 18:57:53 +0600

    refactored

--- core/src/BloomLand/Core/commands/staff/ClearInventoryCommand.php
+++ core/src/BloomLand/Core/commands/staff/ClearInventoryCommand.php
@@ -17,50 +17,58 @@ namespace BloomLand\Core\commands\staff;
             parent::__construct('ci', 'Очистить игровой инвентарь', '/ci', ['clearinventory']);
         }
 
+        public function getPlugin() : Core
+        {
+            return Core::getAPI();
+        }
+
         public function execute(CommandSender $player, string $label, array $args) : bool
         {
-            if (Core::getAPI()->isEnabled()) {
+            if ($this->getPlugin()->isEnabled()) {
+
+                $prefix = $this->getPlugin()->getPrefix();
 
                 $cleared = 0;
 
                 if (isset($args[0])) {
 
-                    if (($target = Core::getAPI()->getServer()->getPlayerByPrefix($args[0])) instanceof BLPlayer) {
+                    if (($target = $this->getPlugin()->getServer()->getPlayerByPrefix($args[0])) instanceof BLPlayer) {
 
                         $contents = array_merge($target->getInventory()->getContents(), $target->getArmorInventory()->getContents());
                         
-                        foreach ($contents as $content) 
-                            $cleared += $content->getCount();
+                        foreach ($contents as $content) $cleared += $content->getCount();
 
                         $target->getInventory()->clearAll();
                         $target->getArmorInventory()->clearAll();
 
-                        $target->sendMessage(' §r> Игрок §b' . $player->getName() . ' §rочистил Ваш ивентарь.');
+                        $target->sendMessage($prefix . 'Игрок §b' . $player->getName() . ' §rочистил Ваш ивентарь.');
                         
                         if ($cleared > 0) 
-                            $player->sendMessage(' §r> Было удалено §b' . $cleared . '§r предметов в инвентаре игрока §c' . $target->getName() . '§r.');
+                            $player->sendMessage($prefix . 'Было удалено §b' . $cleared . '§r предметов в инвентаре игрока §c' . $target->getName() . '§r.');
+
                         else 
-                            $player->sendMessage(' §r> Вы очистили§b пустой§r инвентарь игрока §c' . $target->getName() . '§r.');
+                            $player->sendMessage($prefix . 'Вы очистили§b пустой§r инвентарь игрока §c' . $target->getName() . '§r.');
                     
                     } else {
 
-                        $player->sendMessage(' §r Игрок сейчас §cне в игре§r.');
+                        $player->sendMessage($prefix . 'Игрок сейчас §cне в игре§r.');
 
                     }
 
                 } else {
 
                     $contents = array_merge($player->getInventory()->getContents(), $player->getArmorInventory()->getContents());
-                    foreach ($contents as $content) 
-                        $cleared += $content->getCount();
+                    
+                    foreach ($contents as $content) $cleared += $content->getCount();
 
                     $player->getInventory()->clearAll();
                     $player->getArmorInventory()->clearAll();
                     
                     if ($cleared > 0) 
-                        $player->sendMessage(' §r> Было удалено §b' . $cleared . '§r предметов в инвентаре.');
+                        $player->sendMessage($prefix . 'Было удалено §b' . $cleared . '§r предметов в инвентаре.');
+
                     else 
-                        $player->sendMessage(' §r> Вы очистили§b пустой§r инвентарь.');
+                        $player->sendMessage($prefix . 'Вы очистили§b пустой§r инвентарь.');
 
                 }
                     
@@ -68,6 +76,7 @@ namespace BloomLand\Core\commands\staff;
 
             return true;
         }
+
     }
 
 ?>
