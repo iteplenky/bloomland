f1dfbb19b6210f6f5b8ab3688bdcd00773747a31
Denis Klyuyev <42160062+iteplenky@users.noreply.github.com>
2021-06-27 19:23:04 +0600

    refactored

--- core/src/BloomLand/Core/commands/staff/SayCommand.php
+++ core/src/BloomLand/Core/commands/staff/SayCommand.php
@@ -6,8 +6,6 @@ namespace BloomLand\Core\commands\staff;
 
     use BloomLand\Core\Core;
 
-    use pocketmine\Server;
-
     use pocketmine\command\Command;
     use pocketmine\command\CommandSender;
 
@@ -15,22 +13,36 @@ namespace BloomLand\Core\commands\staff;
     {
         public function __construct()
         {
-            parent::__construct('say', 'Оповестить всех игроков', '/say');
+            parent::__construct('say', 'Оповестить всех игроков', '/say', ['bc', 'broadcast']);
+        }
+
+        public function getPlugin() : Core
+        {
+            return Core::getAPI();
         }
 
         public function execute(CommandSender $player, string $label, array $args) : bool
         {
-            if (Core::getAPI()->isEnabled()) {
+            if ($this->getPlugin()->isEnabled()) {
 
                 if (isset($args[0])) {
+                
                     $msg = implode(' ', $args);
-                    Server::getInstance()->broadcastMessage(' §c§l? §r> Игрок §c' . $player->getName() . '§r вещает: §e' . $msg);
+                    $this->getPlugin()->getServer()->broadcastMessage(' §c§l? §r> Игрок §c' . $player->getName() . '§r вещает: §e' . $msg);
+                
                 }
-                else $player->sendMessage(' §r> Чтобы §bотправить§r всем свое сообщение оно должно иметь какое-то§c содержание§r.');
+
+                else {
+
+                    $player->sendMessage($this->getPlugin()->getPrefix() . 'Чтобы §bотправить§r всем свое сообщение оно должно иметь какое-то§c содержание§r.');
+
+                } 
+
             }
 
             return true;
         }
+
     }
 
 ?>
