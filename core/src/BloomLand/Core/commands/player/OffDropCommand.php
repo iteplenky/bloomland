464992cb46b5c8abd6847118d857ad828c4d876b
Denis Klyuyev <42160062+iteplenky@users.noreply.github.com>
2021-06-27 11:26:13 +0600

    refactored

--- core/src/BloomLand/Core/commands/player/OffDropCommand.php
+++ core/src/BloomLand/Core/commands/player/OffDropCommand.php
@@ -16,23 +16,25 @@ namespace BloomLand\Core\commands\player;
         {
             parent::__construct('offdrop', 'Переместиться на место появления', '/offdrop');
         }
+        
+        // move function to BLPlayer.php
 
         public function execute(CommandSender $player, string $label, array $args): bool
         {
             if (Core::getAPI()->isEnabled()) {
 
                 if ($player instanceof BLPlayer) {
-
-                    if (isset(Core::getAPI()->offdrop[$player->getLowerCaseName()]) and Core::getAPI()->offdrop[$player->getLowerCaseName()] == 1) {
-
-                        $player->sendMessage(Core::getAPI()->getPrefix() . 'Вы теперь §bне можете§r поднимать ресурсы с земли.');
+                    
+                    if (isset(Core::getAPI()->offdrop[$player->getLowerCaseName()])) {
+                        
+                        $player->sendMessage(Core::getAPI()->getPrefix() . 'Вы теперь§b можете§r поднимать ресурсы с земли.');
+                        // unset(Core::getAPI()->offdrop[$player->getLowerCaseName()]);
                         
                     } else {
-
-                        Core::getAPI()->offdrop[$player->getLowerCaseName()] = 1;
-
-                        $player->sendMessage(Core::getAPI()->getPrefix() . 'Вы теперь§b можете§r поднимать ресурсы с земли.');
-
+                     
+                        $player->sendMessage(Core::getAPI()->getPrefix() . 'Вы теперь §bне можете§r поднимать ресурсы с земли.');
+                        // Core::getAPI()->offdrop[$player->getLowerCaseName()] = 1;
+                        
                     }
 
                 }
