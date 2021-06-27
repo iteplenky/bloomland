9726870fa4fa3c2e7d366c9d00063c04f1a0daa7
Denis Klyuyev <42160062+iteplenky@users.noreply.github.com>
2021-06-27 19:50:30 +0600

    reformating

--- core/src/BloomLand/Core/commands/player/SpawnCommand.php
+++ core/src/BloomLand/Core/commands/player/SpawnCommand.php
@@ -7,11 +7,11 @@ namespace BloomLand\Core\commands\player;
     use BloomLand\Core\Core;
     use BloomLand\Core\BLPlayer;
 
-    use BloomLand\Core\utils\API;
-
     use pocketmine\command\Command;
     use pocketmine\command\CommandSender;
-
+    
+    use BloomLand\Core\utils\API;
+    
     use pocketmine\network\mcpe\protocol\OnScreenTextureAnimationPacket;
 
     class SpawnCommand extends Command
@@ -21,9 +21,16 @@ namespace BloomLand\Core\commands\player;
             parent::__construct('spawn', 'Переместиться на место появления', '/spawn');
         }
 
+        public function getPlugin() : Core
+        {
+            return Core::getAPI();
+        }
+
         public function execute(CommandSender $player, string $label, array $args): bool
         {
-            if (Core::getAPI()->isEnabled()) {
+            if ($this->getPlugin()->isEnabled()) {
+
+                $prefix = $this->getPlugin()->getPrefix();
 
                 if ($player instanceof BLPlayer) {
 
@@ -32,9 +39,9 @@ namespace BloomLand\Core\commands\player;
 
                     else {
 
-                        if (Core::getAPI()->getServer()->isOp($player->getName())) {
-				
-			    $config = Core::getAPI()->getConfig();
+                        if ($player->isOp()) {
+                
+                            $config = $this->getPlugin()->getConfig();
 
                             switch ($args[0]) {
                                 
@@ -42,21 +49,20 @@ namespace BloomLand\Core\commands\player;
                                     $config->set('spawnPosition', API::packPositionToRaw($player->getPosition()));
                                     $config->save();
                 
-                                    $player->sendMessage(Core::getPrefix() . $player->translate('spawn.manage.completed'));
+                                    $player->sendMessage($prefix . $player->translate('spawn.manage.completed'));
                                     break;
-                                
 
                                 case 'pos1':
                                     $config->set('spawnPos1', API::packVectorToRaw($player->getLocation()->asVector3()));
 
-                                    $player->sendMessage(Core::getPrefix() . 'Первая точка указана');
+                                    $player->sendMessage($prefix . 'Первая точка указана');
                                     break;
 
                                 case 'pos2':
                                     $config->set('spawnPos2', API::packVectorToRaw($player->getLocation()->asVector3()));
                                     $config->save();
                 
-                                    $player->sendMessage(Core::getPrefix() . 'Безопасная зона создана');
+                                    $player->sendMessage($prefix . 'Безопасная зона создана');
                                     break;
 
                                 default:
@@ -77,30 +83,33 @@ namespace BloomLand\Core\commands\player;
 
         protected static function helpMessage(CommandSender $player): void 
         {
-            $player->sendMessage(Core::getPrefix() . $player->translate('spawn.manage'));
+            $player->sendMessage($this->getPlugin()->getPrefix() . $player->translate('spawn.manage'));
         }
 
         public static function teleport(CommandSender $player): void 
         {
-            $config = Core::getAPI()->getConfig();
+            $config = $this->getPlugin()->getConfig();
 
-	    if ($config->exists('spawnPosition')) {
-                
-                $pos = API::unpackRawLocation($config->get('spawnPosition'));
+            if ($config->exists('spawnPosition')) {
+                    
+                    $pos = API::unpackRawLocation($config->get('spawnPosition'));
 
-                $player->teleport($pos);
+                    $player->teleport($pos);
 
-                $player->sendMessage(Core::getPrefix() . $player->translate('spawn.teleport.message'));
-                $player->sendTitle($player->translate('spawn.teleport.title'), $player->translate('spawn.teleport.subtitle'), 5, 15, 5);
+                    $player->sendMessage($this->getPlugin()->getPrefix() . $player->translate('spawn.teleport.message'));
+                    $player->sendTitle($player->translate('spawn.teleport.title'), $player->translate('spawn.teleport.subtitle'), 5, 15, 5);
 
-                $pk = new OnScreenTextureAnimationPacket();
-                $pk->effectId = 29; 
-                $player->getNetworkSession()->sendDataPacket($pk);
+                    $pk = new OnScreenTextureAnimationPacket();
+                    $pk->effectId = 29; 
+                    $player->getNetworkSession()->sendDataPacket($pk);
 
-            } else 
-                $player->sendMessage(Core::getPrefix() . $player->translate('spawn.teleport.failed'));
+                } else 
+                    $player->sendMessage($this->getPlugin()->getPrefix() . $player->translate('spawn.teleport.failed'));
+
+            }
+            
         }
-	    
+        
     }
 
 ?>
