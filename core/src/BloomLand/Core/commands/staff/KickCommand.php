e182db2fef18656421de1dd2c10f9ace6e46bb5d
Denis Klyuyev <42160062+iteplenky@users.noreply.github.com>
2021-06-27 19:08:45 +0600

    refactored

--- core/src/BloomLand/Core/commands/staff/KickCommand.php
+++ core/src/BloomLand/Core/commands/staff/KickCommand.php
@@ -6,9 +6,6 @@ namespace BloomLand\Core\commands\staff;
 
     use BloomLand\Core\Core;
 
-    use BloomLand\Core\utils\Network;
-    use BloomLand\Core\utils\API;
-
     use pocketmine\command\Command;
     use pocketmine\command\CommandSender;
 
@@ -19,33 +16,51 @@ namespace BloomLand\Core\commands\staff;
             parent::__construct('kick', 'Выгнать нарушителя правил', '/kick');
         }
 
+        public function getPlugin() : Core
+        {
+            return Core::getAPI();
+        }
+
         public function execute(CommandSender $player, string $label, array $args) : bool
         {
-            if (Core::getAPI()->isEnabled()) {
+            if ($this->getPlugin()->isEnabled()) {
+
+                $prefix = $this->getPlugin()->getPrefix();
 
                 if (!empty($args[0])) {
 
                     $name = array_shift($args);
-                    if (!is_null(Core::getAPI()->getServer()->getPlayerByPrefix($name))) {
-                        $intruder = Core::getAPI()->getServer()->getPlayerByPrefix($name);
+
+                    if (!is_null($this->getPlugin()->getServer()->getPlayerByPrefix($name))) {
+
+                        $intruder = $this->getPlugin()->getServer()->getPlayerByPrefix($name);
 
                         if (empty($args[0])) {
-                            $player->sendMessage(Core::getPrefix() . '§eГерой§r, Вы выгнали §b' . $intruder->getName() . '§r, не указав причину, пожалуйста осведомите его о причине.');
+                        
+                            $player->sendMessage($prefix . '§eГерой§r, Вы выгнали §b' . $intruder->getName() . 
+                            '§r, не указав причину, пожалуйста осведомите его о причине.');
                             $args[0] = 'неизвестна';
+                        
                         }
                         
                         
-                        $intruder->kick('§l| §rВы выгнаны игроком §b' . $player->getName() . '§r.' . PHP_EOL . '§l| §rПричина: §e' . implode(" ", $args) . PHP_EOL . PHP_EOL . '§l| §rОставить жалобу: §bvk.com/bl_pe');
-                        Core::getAPI()->getServer()->broadcastMessage(Core::getPrefix() . 'Игрок §b' . $player->getName() . ' §rвыгнал нарушителя §d' . $intruder->getName() . '§r, по причине: §e' . implode(" ", $args));
+                        $intruder->kick('§l| §rВы выгнаны игроком §b' . $player->getName() . '§r.' . PHP_EOL . 
+                        '§l| §rПричина: §e' . implode(" ", $args) . PHP_EOL . PHP_EOL . '§l| §rОставить жалобу: §bvk.com/bl_pe');
+
+                        $this->getPlugin()->getServer()->broadcastMessage($prefix . 'Игрок §b' . $player->getName() . 
+                        ' §rвыгнал нарушителя §d' . $intruder->getName() . '§r, по причине: §e' . implode(" ", $args));
 
                     } else {
 
-                        $player->sendMessage(Core::getAPI()->getPrefix() . 'Игрок сейчас §cне в игре§r.');
+                        $player->sendMessage($this->getPlugin()->getPrefix() . 'Игрок сейчас §cне в игре§r.');
 
                     }
 
-                } else 
-                    $player->sendMessage(Core::getPrefix() . 'Чтобы §bвыгнать нарушителя §rиспользуйте: /kick §r<§7"§bник игрока§7"§r> §r<§eпричина§r>');
+                } else {
+
+                    $player->sendMessage($prefix . 'Чтобы §bвыгнать нарушителя §rиспользуйте: /kick §r<§7"§bник игрока§7"§r> §r<§eпричина§r>');
+                
+                }
 
             } 
             return true;
