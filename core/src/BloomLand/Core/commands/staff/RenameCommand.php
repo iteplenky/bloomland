4df7d61388377343b8d35564a08a64a29fd20603
Denis Klyuyev <42160062+iteplenky@users.noreply.github.com>
2021-06-27 19:19:04 +0600

    refactored

--- core/src/BloomLand/Core/commands/staff/RenameCommand.php
+++ core/src/BloomLand/Core/commands/staff/RenameCommand.php
@@ -13,35 +13,57 @@ namespace BloomLand\Core\commands\staff;
     {
         public function __construct()
         {
-            parent::__construct('rename', 'Восстановить параметры', '/rename');
+            parent::__construct('rename', 'Переименовать предмет', '/rename');
+        }
+
+        public function getPlugin() : Core
+        {
+            return Core::getAPI();
         }
 
         public function execute(CommandSender $player, string $label, array $args) : bool
         {
             if (Core::getAPI()->isEnabled()) {
 
+                $prefix = $this->getPlugin()->getPrefix();
+
                 $heldItem = $player->getInventory()->getItemInHand();
 
                 if ($heldItem->getId() != 0) {
+
                     if (isset($args[0])) {
+                    
                         if (strlen($args[0]) < 12) {
 
                             $heldItem->setCustomName($args[0]);
+                    
                             $player->getInventory()->setItemInHand($heldItem);
                             
-                            $player->sendMessage(' §r> Вы §cсменили название§r предмета на §o' . $args[0] . '§r.');
+                            $player->sendMessage($prefix . 'Вы §cсменили название§r предмета на §o' . $args[0] . '§r.');
 
-                        } else
-                            $player->sendMessage(' §r> Название предмета уж§c слишком велико§r.'); 
-                    } else 
-                        $player->sendMessage(' §r> Чтобы §bпереименовать§r предмет нужно указать его §cновое название§r.'); 
-                } else 
-                    $player->sendMessage(' §r> Чтобы §bпереименовать§r предмет нужно взять его в §cруку§r.'); 
+                        } else {
+
+                            $player->sendMessage($prefix . 'Название предмета уж§c слишком велико§r.'); 
+
+                        }
+                        
+                    } else {
+
+                        $player->sendMessage($prefix . 'Чтобы §bпереименовать§r предмет нужно указать его §cновое название§r.'); 
+
+                    }
+                    
+                } else {
+
+                    $player->sendMessage($prefix . 'Чтобы §bпереименовать§r предмет нужно взять его в §cруку§r.'); 
+
+                }
 
             }
 
             return true;
         }
+
     }
 
 ?>
