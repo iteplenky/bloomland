2b0b060dbff2b627b00e6e6b222b8bfbb38e178d
Denis Klyuyev <42160062+iteplenky@users.noreply.github.com>
2021-06-27 11:44:13 +0600

    refactored

--- core/src/BloomLand/Core/commands/player/SeeMoneyCommand.php
+++ core/src/BloomLand/Core/commands/player/SeeMoneyCommand.php
@@ -22,31 +22,33 @@ namespace BloomLand\Core\commands\player;
         {
             if (Core::getAPI()->isEnabled()) {
 
+                $prefix = Core::getAPI()->getPrefix();
+
                 if (isset($args[0])) {
 
                     if (($target = Core::getAPI()->getServer()->getPlayerByPrefix($args[0])) instanceof BLPlayer) {
 
                         if ($target->getLowerCaseName() == $player->getLowerCaseName()) {
 
-                            $player->sendMessage(Core::getAPI()->getPrefix() . 'Вы пытаетесь просмотреть §bсобственный баланс§r.');
-                            $player->sendMessage(Core::getAPI()->getPrefix() . $player->translate('coins.get', [number_format($player->getMoney(), 0, '', ' ')]));
+                            $player->sendMessage($prefix . 'Вы пытаетесь просмотреть §bсобственный баланс§r.');
+                            $player->sendMessage($prefix . $player->translate('coins.get', [number_format($player->getMoney(), 0, '', ' ')]));
                             
                         } else {
 
-                            $player->sendMessage(Core::getAPI()->getPrefix() . 'Игровой баланс игрока §b' . $target->getName() . ' §r> §b' . number_format($target->getMoney(), 0, '', ' ') . ' §rмонет.');
-                            $target->sendMessage(Core::getAPI()->getPrefix() . 'Игрок §b' . $player->getName() . ' §rпросмотрел Ваш игровой баланс.');
+                            $player->sendMessage($prefix . 'Игровой баланс игрока §b' . $target->getName() . ' §r> §b' . number_format($target->getMoney(), 0, '', ' ') . ' §rмонет.');
+                            $target->sendMessage($prefix . 'Игрок §b' . $player->getName() . ' §rпросмотрел Ваш игровой баланс.');
                             
                         }
                         
                     } else {
                         
-                        $player->sendMessage(Core::getAPI()->getPrefix() . 'Игрок сейчас §cне в игре§r.');
+                        $player->sendMessage($prefix . 'Игрок сейчас §cне в игре§r.');
                         
                     }
                     
                 } else {
                     
-                    $player->sendMessage(Core::getAPI()->getPrefix() . 'Чтобы §bпросмотреть баланс§r другого игрока, используйте: /seemoney <§bигрок§r>');
+                    $player->sendMessage($prefix . 'Чтобы §bпросмотреть баланс§r другого игрока, используйте: /seemoney <§bигрок§r>');
                     
                 }
                     
@@ -54,6 +56,7 @@ namespace BloomLand\Core\commands\player;
 
             return true;
         }
+        
     }
 
 ?>
