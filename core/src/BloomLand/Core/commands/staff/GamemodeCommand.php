6d280112c549e62db79ab54c25a93ec0a416a544
Denis Klyuyev <42160062+iteplenky@users.noreply.github.com>
2021-06-27 19:04:14 +0600

    refactored

--- core/src/BloomLand/Core/commands/staff/GamemodeCommand.php
+++ core/src/BloomLand/Core/commands/staff/GamemodeCommand.php
@@ -7,10 +7,10 @@ namespace BloomLand\Core\commands\staff;
     use BloomLand\Core\Core;
     use BloomLand\Core\BLPlayer;
 
-    use pocketmine\player\GameMode;
-
     use pocketmine\command\Command;
     use pocketmine\command\CommandSender;
+    
+    use pocketmine\player\GameMode;
 
     class GamemodeCommand extends Command
     {
@@ -19,40 +19,49 @@ namespace BloomLand\Core\commands\staff;
             parent::__construct('gm', 'Сменить игровой режим', '/gm', ['gamemode']);
         }
 
+        public function getPlugin() : Core
+        {
+            return Core::getAPI();
+        }
+
         public function execute(CommandSender $player, string $label, array $args) : bool
         {
-            if (Core::getAPI()->isEnabled()) {
+            if ($this->getPlugin()->isEnabled()) {
+
+                $prefix = $this->getPlugin()->getPrefix();
 
                 if (isset($args[0])) {
 
-                    if (($target = Core::getAPI()->getServer()->getPlayerByPrefix($args[0])) instanceof BLPlayer) {
+                    if (($target = $this->getPlugin()->getServer()->getPlayerByPrefix($args[0])) instanceof BLPlayer) {
 
                         if ($target->getLowerCaseName() == $player->getLowerCaseName()) {
 
-                            if ($player->isCreative()) {
+                            if ($player->isSurvival()) {
 
-                                $player->setGamemode(GameMode::SURVIVAL());
-                                $player->sendMessage(Core::getAPI()->getPrefix() . 'Вы сменили свой режим на §cВыживание§r.');
+                                $player->setGamemode(GameMode::CREATIVE());
+                                $player->sendMessage($prefix . 'Вы сменили свой режим на §bТворческий§r.');
                 
                             } else {
                 
-                                $player->setGamemode(GameMode::CREATIVE());
-                                $player->sendMessage(Core::getAPI()->getPrefix() . 'Вы сменили свой режим на §bТворческий§r.');
+                                $player->setGamemode(GameMode::SURVIVAL());
+                                $player->sendMessage($prefix . 'Вы сменили свой режим на §cВыживание§r.');
+
                             }
                             
                         } else {
 
-                            if ($target->isCreative()) {
+                            if ($target->isSurvival()) {
 
-                                $target->setGamemode(GameMode::SURVIVAL());
-                                $target->sendMessage(Core::getAPI()->getPrefix() . 'Игрок §b' . $player->getName() . ' §rсменил Вам режим на §cВыживание§r.');
-                                $player->sendMessage(Core::getAPI()->getPrefix() . 'Вы сменили игроку §b' . $target->getName() . ' §rигровой на §cВыживание§r.');
+                                $target->setGamemode(GameMode::CREATIVE());
+                                $target->sendMessage($prefix . 'Игрок §b' . $player->getName() . ' §rсменил Вам режим на §bТворческий§r.');
+                                $player->sendMessage($prefix . 'Вы сменили игроку §b' . $target->getName() . ' §rигровой режим на §bТворческий§r.');
                                 
                             } else {
                 
-                                $target->setGamemode(GameMode::CREATIVE());
-                                $target->sendMessage(Core::getAPI()->getPrefix() . 'Игрок §b' . $player->getName() . ' §rсменил Вам режим на §bТворческий§r.');
-                                $player->sendMessage(Core::getAPI()->getPrefix() . 'Вы сменили игроку §b' . $target->getName() . ' §rигровой режим на §bТворческий§r.');
+                                $target->setGamemode(GameMode::SURVIVAL());
+                                $target->sendMessage($prefix . 'Игрок §b' . $player->getName() . ' §rсменил Вам режим на §cВыживание§r.');
+                                $player->sendMessage($prefix . 'Вы сменили игроку §b' . $target->getName() . ' §rигровой на §cВыживание§r.');
+
                             }
 
                         }
@@ -60,21 +69,21 @@ namespace BloomLand\Core\commands\staff;
     
                     } else {
 
-                        $player->sendMessage(Core::getAPI()->getPrefix() . 'Игрок §cне в игре§r.');
+                        $player->sendMessage($prefix . 'Игрок §cне в игре§r.');
 
                     }
 
                 } else {
 
-                    if ($player->isCreative()) {
+                    if ($player->isSurvival()) {
 
-                        $player->setGamemode(GameMode::SURVIVAL());
-                        $player->sendMessage(Core::getAPI()->getPrefix() . 'Вы сменили свой режим на §cВыживание§r.');
+                        $player->setGamemode(GameMode::CREATIVE());
+                        $player->sendMessage($prefix . 'Вы сменили свой режим на §bТворческий§r.');
         
                     } else {
-        
-                        $player->setGamemode(GameMode::CREATIVE());
-                        $player->sendMessage(Core::getAPI()->getPrefix() . 'Вы сменили свой режим на §bТворческий§r.');
+                        $player->setGamemode(GameMode::SURVIVAL());
+                        $player->sendMessage($prefix . 'Вы сменили свой режим на §cВыживание§r.');
+                        
                     }
 
                 }
@@ -83,6 +92,7 @@ namespace BloomLand\Core\commands\staff;
 
             return true;
         }
+
     }
 
 ?>
