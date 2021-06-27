24f5cc14d965845fdbe40a8fcdfe75d1ca980b0f
Denis Klyuyev <42160062+iteplenky@users.noreply.github.com>
2021-06-27 19:35:09 +0600

    reformat

--- core/src/BloomLand/Core/commands/staff/ToSpawnCommand.php
+++ core/src/BloomLand/Core/commands/staff/ToSpawnCommand.php
@@ -6,11 +6,11 @@ namespace BloomLand\Core\commands\staff;
 
     use BloomLand\Core\Core;
     use BloomLand\Core\BLPlayer;
-    use BloomLand\Core\utils\API;
-
+    
     use pocketmine\command\Command;
     use pocketmine\command\CommandSender;
-
+    
+    use BloomLand\Core\utils\API;
     use pocketmine\network\mcpe\protocol\OnScreenTextureAnimationPacket;
 
     class ToSpawnCommand extends Command
@@ -20,37 +20,44 @@ namespace BloomLand\Core\commands\staff;
             parent::__construct('tospawn', 'Переместиться на место появления', '/tospawn', ['tpspawn']);
         }
 
-        public function execute(CommandSender $player, string $label, array $args): bool
+        public function getPlugin() : Core
         {
-            if (Core::getAPI()->isEnabled()) {
+            return Core::getAPI();
+        }
+
+        public function execute(CommandSender $player, string $label, array $args) : bool
+        {
+            if ($this->getPlugin()->isEnabled()) {
+
+                $prefix = $this->getPlugin()->getPrefix();
 
                 if ($player instanceof BLPlayer) {
 
                     if (isset($args[0])) {
 
-                        if (($target = Core::getAPI()->getServer()->getPlayerByPrefix($args[0])) instanceof BLPlayer) {
+                        if (($target = $this->getPlugin()->getServer()->getPlayerByPrefix($args[0])) instanceof BLPlayer) {
     
                             if ($target->getLowerCaseName() == $player->getLowerCaseName()) {
     
-                                $player->sendMessage(Core::getAPI()->getPrefix() . 'Вы переместили §bсамого себя§r на спавн§r.');
+                                $player->sendMessage($prefix . 'Вы переместили §bсамого себя§r на спавн§r.');
                                 self::teleport($player);
                                 
                             } else {
     
-                                $target->sendMessage(Core::getAPI()->getPrefix() . 'Игрок §b' . $player->getName() . ' §rперенес вас на точку §bвозрождения§r при помощи команды.');
-                                $player->sendMessage(Core::getAPI()->getPrefix() . 'Вы перенесли §b' . $target->getName() . ' §rна точку §bвозрождения §rпри помощи команды.');
+                                $target->sendMessage($prefix . 'Игрок §b' . $player->getName() . ' §rперенес вас на точку §bвозрождения§r при помощи команды.');
+                                $player->sendMessage($prefix . 'Вы перенесли §b' . $target->getName() . ' §rна точку §bвозрождения §rпри помощи команды.');
                                 self::teleport($target);
                             }
                             
                         } else {
                             
-                            $player->sendMessage(Core::getAPI()->getPrefix() . 'Игрок сейчас §cне в игре§r.');
+                            $player->sendMessage($prefix . 'Игрок сейчас §cне в игре§r.');
                             
                         }
                         
                     } else {
                         
-                        $player->sendMessage(Core::getAPI()->getPrefix() . 'Чтобы переместестить §bдругого игрока§r на спавн, используйте: /tpspawn <§bигрок>§r');
+                        $player->sendMessage($prefix . 'Чтобы переместестить §bдругого игрока§r на спавн, используйте: /tospawn <§bигрок>§r');
                         
                     }
 
@@ -58,14 +65,14 @@ namespace BloomLand\Core\commands\staff;
 
                     if (isset($args[0])) {
 
-                        if (($target = Core::getAPI()->getServer()->getPlayerByPrefix($args[0])) instanceof BLPlayer) {
+                        if (($target = $this->getPlugin()->getServer()->getPlayerByPrefix($args[0])) instanceof BLPlayer) {
 
-                            Core::getAPI()->getServer()->getLogger()->notice('Игрок ' . $target->getName() . ' перенесен в точку возраждения.');
+                            $this->getPlugin()->getServer()->getLogger()->notice('Игрок ' . $target->getName() . ' перенесен в точку возраждения.');
                             self::teleport($target);
 
                         }  else {
                             
-                            $player->sendMessage(Core::getAPI()->getPrefix() . 'Игрок сейчас §cне в игре§r.');
+                            $player->sendMessage($prefix . 'Игрок сейчас §cне в игре§r.');
                             
                         }
 
@@ -78,26 +85,31 @@ namespace BloomLand\Core\commands\staff;
             return true;
         }
 
-        public static function teleport(BLPlayer $player): void 
+        public static function teleport(BLPlayer $player) : void 
         {
-            $config = Core::getAPI()->getConfig();
+            $config = $this->getPlugin()->getConfig();
 
-		    if ($config->exists('spawnPosition')) {
+            if ($config->exists('spawnPosition')) {
                 
                 $pos = API::unpackRawLocation($config->get('spawnPosition'));
 
                 $player->teleport($pos);
 
-                $player->sendMessage(Core::getPrefix() . $player->translate('spawn.teleport.message'));
+                $player->sendMessage($this->getPlugin()->getPrefix() . $player->translate('spawn.teleport.message'));
                 $player->sendTitle($player->translate('spawn.teleport.title'), $player->translate('spawn.teleport.subtitle'), 5, 15, 5);
 
                 $pk = new OnScreenTextureAnimationPacket();
                 $pk->effectId = 29; // or 27
                 $player->getNetworkSession()->sendDataPacket($pk);
 
-            } else 
-                $player->sendMessage(Core::getPrefix() . $player->translate('spawn.teleport.failed'));
+            } else {
+
+                $player->sendMessage($this->getPlugin()->getPrefix() . $player->translate('spawn.teleport.failed'));
+
+            }
+        
         }
+    
     }
 
 ?>
