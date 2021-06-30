facb2d7b04e8b5847448ab74c40bf2dd55e71a16
Denis Klyuyev <42160062+iteplenky@users.noreply.github.com>
2021-06-30 11:32:05 +0600

    tabs fixed

--- auth/src/iteplenky/RegUI/command/RegisterCommand.php
+++ auth/src/iteplenky/RegUI/command/RegisterCommand.php
@@ -4,46 +4,46 @@
 namespace iteplenky\RegUI\command;
 
 
-	use iteplenky\RegUI\Main;
-	use iteplenky\RegUI\form\RegisterForm;
+    use iteplenky\RegUI\Main;
+    use iteplenky\RegUI\form\RegisterForm;
 
-	use pocketmine\command\Command;
-	use pocketmine\command\CommandSender;
+    use pocketmine\command\Command;
+    use pocketmine\command\CommandSender;
 
-	class RegisterCommand extends Command
-	{
-		
-		/** @var Main */
-		private $main;
-		
-		public function __construct(Main $main, string $name, string $description, string $permission)
-		{
-			$this->main = $main;
-			
-			parent::__construct($name, $description);
-			$this->setPermission($permission);
-		}
-		
-		/**
-		 * @param CommandSender $sender
-		 * @param string $label
-		 * @param array $args
-		 * @return void
-		 */
-		public function execute(CommandSender $sender, string $label, array $args) : void
-		{
-			if (!$this->testPermission($sender)) return;
-			
-			if ($this->main->isRegistered($sender->getName())) {
+    class RegisterCommand extends Command
+    {
+        
+        /** @var Main */
+        private $main;
+        
+        public function __construct(Main $main, string $name, string $description, string $permission)
+        {
+            $this->main = $main;
+            
+            parent::__construct($name, $description);
+            $this->setPermission($permission);
+        }
+        
+        /**
+         * @param CommandSender $sender
+         * @param string $label
+         * @param array $args
+         * @return void
+         */
+        public function execute(CommandSender $sender, string $label, array $args) : void
+        {
+            if (!$this->testPermission($sender)) return;
+            
+            if ($this->main->isRegistered($sender->getName())) {
 
-				$sender->sendMessage($this->main->config["error"]["registered"]);
-				return;
-			
-			}
-			
-			$sender->sendForm(new RegisterForm($this->main, 'Регистрация'));
-		}
-	
-	}
+                $sender->sendMessage($this->main->config["error"]["registered"]);
+                return;
+            
+            }
+            
+            $sender->sendForm(new RegisterForm($this->main, 'Регистрация'));
+        }
+    
+    }
 
 ?>
