653fb3645d3322b96cb85f9e4780adfa770aa8e1
Denis Klyuyev <42160062+iteplenky@users.noreply.github.com>
2021-06-30 11:36:48 +0600

    getDatabase implemented

--- auth/src/iteplenky/RegUI/Main.php
+++ auth/src/iteplenky/RegUI/Main.php
@@ -55,10 +55,16 @@ namespace iteplenky\RegUI;
             $this->config = $this->getConfig()->getAll(); //получения конфига в виде массива
             
             $this->database = new \SQLite3($this->getDataFolder() . 'database.db'); //создание базы данных
-            $this->database->exec("CREATE TABLE IF NOT EXISTS players(player TEXT PRIMARY KEY COLLATE NOCASE, password TEXT, IpAddress TEXT, vkId TEXT)"); //создание таблицы в базе данных
+
+            $this->getDatabase()->exec("CREATE TABLE IF NOT EXISTS players(player TEXT PRIMARY KEY COLLATE NOCASE, password TEXT, IpAddress TEXT, vkId TEXT)"); //создание таблицы в базе данных
             
             $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this); //регистрация событий
             $this->getLogger()->notice('Игроков в базе: ' . $this->getPlayersCount());
+        }
+
+        public function getDatabase() : \SQLite3
+        {
+            return $this->database;
         }
         
         /**
@@ -68,10 +74,13 @@ namespace iteplenky\RegUI;
          */
         public function isRegistered(string $player) : bool
         {
-            $db = $this->database->prepare("SELECT player FROM players WHERE player =:player;");
+            $db = $this->getDatabase()->prepare("SELECT player FROM players WHERE player =:player;");
+
             $db->bindValue(":player", strtolower($player), SQLITE3_TEXT);
+            
             $result = $db->execute();
             $result = $result->fetchArray(SQLITE3_ASSOC);
+            
             $db->close();
             
             if (empty($result)) return false;
@@ -98,13 +107,16 @@ namespace iteplenky\RegUI;
         public function setLogined(string $player, bool $value = true) : void
         {
             switch($value) {
+                
                 case true:
                     $this->logined[] = strtolower($player);
                 break;
+
                 case false:
                     unset($this->logined[array_search(strtolower($player), $this->logined, true)]);
                 break;
             }
+
         }
         
         /**
@@ -115,11 +127,13 @@ namespace iteplenky\RegUI;
          */
         public function register(Player $player, string $password, string $vkId) : void
         {
-            $db = $this->database->prepare("INSERT OR REPLACE INTO players(player, password, IpAddress, vkId) VALUES(:player, :password, :IpAddress, :vkId)");
+            $db = $this->getDatabase()->prepare("INSERT OR REPLACE INTO players(player, password, IpAddress, vkId) VALUES(:player, :password, :IpAddress, :vkId)");
+
             $db->bindValue(":player", strtolower($player->getName()), SQLITE3_TEXT);
             $db->bindValue(":password", $password, SQLITE3_TEXT);
             $db->bindValue(":IpAddress", $player->getNetworkSession()->getIp(), SQLITE3_TEXT);
             $db->bindValue(":vkId", strtolower($vkId), SQLITE3_TEXT);
+            
             $db->execute();
             $db->close();
         }
@@ -130,11 +144,15 @@ namespace iteplenky\RegUI;
          */
         public function getData(string $player) : array
         {
-            $db = $this->database->prepare("SELECT * FROM players WHERE player=:player;");
+            $db = $this->getDatabase()->prepare("SELECT * FROM players WHERE player=:player;");
+
             $db->bindValue(":player", strtolower($player));
+            
             $end = $db->execute();
             $array = $end->fetchArray(SQLITE3_ASSOC);
+            
             $db->close();
+            
             return $array;
         }
         
@@ -145,9 +163,11 @@ namespace iteplenky\RegUI;
          */
         public function changepassword(string $player, string $password) : void
         {
-            $db = $this->database->prepare("UPDATE players SET password=:newpassword WHERE player=:player;");
+            $db = $this->getDatabase()->prepare("UPDATE players SET password=:newpassword WHERE player=:player;");
+
             $db->bindValue(":newpassword", $password);
             $db->bindValue(":player", strtolower($player));
+            
             $db->execute();
             $db->close();
         }
@@ -158,15 +178,16 @@ namespace iteplenky\RegUI;
          */
         public function logout(string $player) : void
         {
-            $db = $this->database->prepare("DELETE FROM players WHERE player=:player;");
+            $db = $this->getDatabase()->prepare("DELETE FROM players WHERE player=:player;");
+
             $db->bindValue(":player", strtolower($player));
+            
             $db->execute();
             $db->close();
             
-            if ($this->isLogined($player))
-                $this->setLogined($player, false);
+            if ($this->isLogined($player)) $this->setLogined($player, false);
             
-            if ($this->getServer()->getPlayerByPrefix($player) !== null)
+            if ($this->getServer()->getPlayerByPrefix($player) !== null) 
                 $this->getServer()->getPlayerByPrefix($player)->kick($this->config["success"]["logout"]);
             
         }
@@ -178,9 +199,11 @@ namespace iteplenky\RegUI;
          */
         public function updateIP(string $player, string $ip) : void
         {
-            $db = $this->database->prepare("UPDATE players SET IpAddress=:IpAddress WHERE player=:player;");
+            $db = $this->getDatabase()->prepare("UPDATE players SET IpAddress=:IpAddress WHERE player=:player;");
+
             $db->bindValue(":IpAddress", $ip);
             $db->bindValue(":player", strtolower($player));
+            
             $db->execute();
             $db->close();
         }
@@ -190,10 +213,11 @@ namespace iteplenky\RegUI;
          */
         public function getPlayersCount() : int
         {
-            $rows = $this->database->query("SELECT COUNT(*) as count FROM players");
+            $rows = $this->getDatabase()->query("SELECT COUNT(*) as count FROM players");
             $count = $rows->fetchArray(SQLITE3_ASSOC);
             return $count['count'];
         }
+
     }
 
 ?>
