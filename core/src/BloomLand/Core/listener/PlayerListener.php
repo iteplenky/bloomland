e0b38fce63ce06939d10de5b380a06337e82b303
Denis Klyuyev <42160062+iteplenky@users.noreply.github.com>
2021-06-30 11:21:38 +0600

    refactored

--- core/src/BloomLand/Core/listener/PlayerListener.php
+++ core/src/BloomLand/Core/listener/PlayerListener.php
@@ -4,86 +4,55 @@
 namespace BloomLand\Core\listener;
 
 
-    use BloomLand\Core\Core;
-    use BloomLand\Core\BLPlayer;
-    use BloomLand\Core\AntiCheat;
-    use BloomLand\Core\task\HealthTask;
-    use BloomLand\Core\task\PickupTask;
-    use BloomLand\Core\item\Hammer;
-    use BloomLand\Core\task\TagTask; 
+    use BloomLand\Core\{
+        Core,
+        BLPlayer,
+        AntiCheat,
 
-    use BloomLand\Core\sqlite3\SQLite3;
- 
-    use BloomLand\Chat\ChatFilter; 
-    use BloomLand\Core\base\Ban;
+        base\Ban,
 
-    use BloomLand\Core\base\Economy;
+        scoreboard\ScoreboardFactory,
 
-    use BloomLand\Core\utils\API;
-    use pocketmine\world\sound\AnvilUseSound;
+        task\PickupTask,
+        task\TagTask,
 
+        item\Hammer,
+
+        utils\API
+    };
+  
     use pocketmine\event\Listener;
-
-    use pocketmine\block\VanillaBlocks;
-    // use pocketmine\math\Vector3;
 
     use pocketmine\event\player as event;
 
+    
+    use pocketmine\event\server\{
+        DataPacketReceiveEvent,
+        DataPacketSendEvent
+    };
+    
+    use pocketmine\network\mcpe\protocol\{
+        TextPacket,
+        AvailableCommandsPacket,
+        ResourcePackDataInfoPacket,
+        EmotePacket
+    };
+    
     use pocketmine\event\entity\EntityDamageEvent;
     use pocketmine\event\entity\EntityDamageByEntityEvent;
-
-    use pocketmine\utils\TextFormat;
-
-    use pocketmine\network\mcpe\protocol\OnScreenTextureAnimationPacket;
-    use BloomLand\Core\scoreboard\ScoreboardFactory;
-    use pocketmine\math\Vector3;
-    use pocketmine\item\VanillaItems;
-    use pocketmine\block\BlockLegacyIds;
-    use pocketmine\network\mcpe\protocol\UpdateBlockPacket;
-    use pocketmine\network\mcpe\protocol\types\RuntimeBlockMapping;
-    use pocketmine\network\mcpe\protocol\types\DeviceOS;
-    use pocketmine\network\mcpe\protocol\LoginPacket;
-    use pocketmine\network\mcpe\protocol\ProtocolInfo;
+    
     use pocketmine\event\block\BlockBreakEvent;
-    use pocketmine\item\ItemFactory;
-    use pocketmine\block\BlockFactory;
-    use pocketmine\Server;
-    use pocketmine\player\GameMode;
-    use pocketmine\command\ConsoleCommandSender;
-    use pocketmine\event\server\DataPacketReceiveEvent;
-    use pocketmine\network\mcpe\protocol\EmotePacket;
+    
     use pocketmine\event\server\CommandEvent;
-    use pocketmine\event\server\DataPacketSendEvent;
-    use pocketmine\network\mcpe\protocol\AvailableCommandsPacket;
-    use pocketmine\network\mcpe\protocol\TextPacket;
-    use _64FF00\PurePerms\PurePerms;
-    use pocketmine\network\mcpe\protocol\AnimateEntityPacket;
-    use pocketmine\scheduler\ClosureTask;
 
-    use muqsit\fakeplayer\Loader;
-
-use pocketmine\resourcepacks\ResourcePack;
-use pocketmine\resourcepacks\ResourcePackManager;
-
-use pocketmine\network\mcpe\protocol\types\resourcepacks\ResourcePackType;
-use pocketmine\network\mcpe\protocol\ResourcePackClientResponsePacket;
-use pocketmine\network\mcpe\protocol\ResourcePackChunkRequestPacket;
-use pocketmine\network\mcpe\protocol\ResourcePackChunkDataPacket;
-use pocketmine\network\mcpe\protocol\ResourcePackDataInfoPacket;
-use pocketmine\network\mcpe\protocol\ResourcePacksInfoPacket;
-use pocketmine\network\mcpe\protocol\ResourcePackStackPacket;
-use BloomLand\Crates\entity\Backpack;
-use BloomLand\Crates\animation\BackpackAnimation;
     class PlayerListener implements Listener
     {
-        private $afk;
         private $plugin;
-        private $time = [];
-
-        public static $tm = [];
-        public static $first = []; 
-        public static $new = []; 
-
+        
+        private $afk;
+        
+        public static $first, $tm, $new = []; 
+        
         /** @var string[] */
         private $devices = [];
 
@@ -105,79 +74,76 @@ use BloomLand\Crates\animation\BackpackAnimation;
 
         public function handlePreLogin(event\PlayerPreLoginEvent $event) : void
         {
-            // if ($event->getPlayerInfo()->getUsername() != 'iteplenky8501') {
-
-            //     $event->setKickReason(1, '§eсервер в режиме разработки..');
-                
-            // } 
-
-            // print_r($event->getPlayerInfo()->getExtraData());
-
             $user = strtolower($event->getPlayerInfo()->getUsername());
+
             $deviceOS = $event->getPlayerInfo()->getExtraData()['DeviceOS'];
 
             static $device = [
-				'Неизвестно',
-				'Android',
-				'iOS',
-				'macOS',
-				'FireOS',
-				'GearVR',
-				'HoloLens',
-				'Windows 10',
-				'Windows',
-				'Dedicated',
-				'tvOS',
-				'Orbis',
-				'NX'
-			];
-			$this->devices[$user] = $device[$deviceOS] ?? 'Неизвестно';
+                'Неизвестно',
+                'Android',
+                'iOS',
+                'macOS',
+                'FireOS',
+                'GearVR',
+                'HoloLens',
+                'Windows 10',
+                'Windows',
+                'Dedicated',
+                'tvOS',
+                'PlayStation',
+                'Nintendo',
+                'Xbox',
+                'Windows Phone'
+            ];
+            $this->devices[$user] = $device[$deviceOS] ?? 'Неизвестно';
             
-            if (Ban::isBanned($user)) 
+            if (Ban::isBanned($user)) {
+
                 $event->setKickReason(1, '§l| §rВы заблокированы игроком §a' . Ban::get($user, 'sender') . '§7.' . PHP_EOL . 
                 '§l| §r§fПричина: §e' . Ban::get($user, 'reason') . PHP_EOL . PHP_EOL . '§l| §rОставить жалобу: §bvk.com/bl_pe');
+
+            }
             
         }
 
         public function handleJoin(event\PlayerJoinEvent $event) : void
         {
-            $player = $event->getPlayer();
-
             $event->setJoinMessage(null);
+            
+            $player = $event->getPlayer();
 
             $ping = $player->getNetworkSession()->getPing();
 
             $status = ' §cОчень Плохой';
 
-            if ($ping < 60)
-                $status = ' §aОтличный';
+            if ($ping < 60) $status = ' §aОтличный';
                 
-            elseif ($ping < 140)
-                $status = ' §cХороший';
+            elseif ($ping < 140) $status = ' §cХороший';
                 
-            elseif ($ping < 250)
-                $status = ' §eНестабильный';
+            elseif ($ping < 250) $status = ' §eНестабильный';
                 
-            elseif ($ping < 400)
-                $status = ' §cПлохой';
+            elseif ($ping < 400) $status = ' §cПлохой';
 
             $this->getPlugin()->getServer()->broadcastPopup(' §f[§b+§f] §f' . $player->getName());
+
             $this->getPlugin()->getLogger()->info('§7Игрок §b' . $player->getName() . ' §7зашел со статусом ' . $status . '§7.');
             
             if ($player instanceof BLPlayer) {
-
-                // $pp = Core::getAPI()->getServer()->getPluginManager()->getPlugin('PurePerms');
-
-                // $pp->updatePermissions($player);
-
-                $player->loadPlayer($this->getPlugin()->getDatabase());
-                
-                // $player->setLanguage(SQLite3::getStringValue($player->getLowerCaseName(), 'lang'));
 
                 $player->joinTime = time();
                 $player->device = $this->devices[$player->getLowerCaseName()];
 
                 unset($this->devices[$player->getLowerCaseName()]);
+
+                $cases = [2, 0, 1, 1, 1, 2];
+                
+                $after = [' §rигрок', ' §rигрока', ' §rигроков'];
+                
+                $number = count($this->getPlugin()->getServer()->getOnlinePlayers());
+
+                $player->loadPlayer($this->getPlugin()->getDatabase());
+                
+                // $player->setLanguage(SQLite3::getStringValue($player->getLowerCaseName(), 'lang'));
                
                 $player->sendMessage(' ');
                 $player->sendMessage(' ');
@@ -187,14 +153,9 @@ use BloomLand\Crates\animation\BackpackAnimation;
                 $player->sendMessage(' ');
                 $player->sendMessage(' ');
                 $player->sendMessage(' ');
-
-                $cases = [2, 0, 1, 1, 1, 2];
-                
-                $after = [' §rигрок', ' §rигрока', ' §rигроков'];
-                
-                $number = count($this->getPlugin()->getServer()->getOnlinePlayers());
                
-                $player->sendMessage(' §r> Сейчас в сети: §b' . $number . $after[($number % 100 > 4 && $number % 100 < 20) ? 2 : $cases[min($number % 10, 5)]] . '.');
+                $player->sendMessage(' §r> Сейчас в сети: §b' . $number . 
+                $after[($number % 100 > 4 && $number % 100 < 20) ? 2 : $cases[min($number % 10, 5)]] . '.');
                 
                 $player->sendMessage(' ');
                 
@@ -204,23 +165,9 @@ use BloomLand\Crates\animation\BackpackAnimation;
                 $player->sendMessage(' §r> Добро пожаловать на сервер ');
                 $player->sendMessage(' ');
 
-                // $player->sendTitle('', '', 20, 30, 5);
+                ScoreboardFactory::createScoreboard($player);
 
-                // $player->getLocation()->getWorld()->addSound($player->getLocation(), new GenericSound(LevelSoundEventPacket::SOUND_RECORD_CHIRP)); 
-                //SOUND_RECORD_CHIRP // mal //stal
-                
-                $pk = new OnScreenTextureAnimationPacket();
-                $pk->effectId = 27; 
-                $player->getNetworkSession()->sendDataPacket($pk);
-
-                if ($player->isConnected()) {
-
-                    ScoreboardFactory::createScoreboard($player);
-
-                }
-                
                 Core::getAPI()->getScheduler()->scheduleRepeatingTask(new TagTask($player, $this), 20);
-                // Core::getAPI()->getScheduler()->scheduleDelayedTask(new HealthTask($player), 20);
 
             }
 
@@ -242,39 +189,20 @@ use BloomLand\Crates\animation\BackpackAnimation;
 
             }
 
-            
-
-            // if ($entity->getAbsorption() > 0) 
-            //     $mode = round($entity->getHealth()) . ' ' . round($entity->getAbsorption()) . '';
-
-            // else 
-            //     $mode = round($entity->getHealth()) . '';
-
-            // $entity->setScoreTag($mode);
-
-            // if ($damager->getAbsorption() > 0) 
-            //     $mode = round($damager->getHealth()) . ' ' . round($damager->getAbsorption()) . '';
-
-            // else 
-            //     $mode = round($damager->getHealth()) . '';
-
-            // $damager->setScoreTag($mode);
-
         }
 
         public function handleEntityDamageEvent(EntityDamageEvent $event) : void
         {
-            if ($event->getCause() !== EntityDamageEvent::CAUSE_SUFFOCATION)
-                return;
+            if ($event->getCause() !== EntityDamageEvent::CAUSE_SUFFOCATION) return;
 
             $entity = $event->getEntity();
-            if (!$entity instanceof BLPlayer)
-                return;
+
+            if (!$entity instanceof BLPlayer) return;
 
             $world = $entity->getWorld();
             $vec = $entity->getPosition()->floor();
 
-            foreach ([2,3,4,5] as $_ => $face) {
+            foreach ([2, 3, 4, 5] as $_ => $face) { // ?vector sides
                 
                 $blockVec = $vec->getSide($face);
 
@@ -292,7 +220,7 @@ use BloomLand\Crates\animation\BackpackAnimation;
 
         public function handleCommandEvent(CommandEvent $event) : void
         {
-            $explode = explode(" ", $event->getCommand());
+            $explode = explode(' ', $event->getCommand());
             $commands = $this->getPlugin()->getServer()->getCommandMap()->getCommands();
         
             if (isset($commands[$explode[0]]))
@@ -309,15 +237,14 @@ use BloomLand\Crates\animation\BackpackAnimation;
             
             }
             
-            $event->setCommand(implode(" ", $explode));
+            $event->setCommand(implode(' ', $explode));
         }
 
         public function handleBlockBreak(BlockBreakEvent $event) : void
         {
             $player = $event->getPlayer();
         
-            if (!$player->hasFiniteResources())
-                return;
+            if (!$player->hasFiniteResources()) return;
     
             $blockPos = $event->getBlock()->getPos();
 
@@ -351,7 +278,7 @@ use BloomLand\Crates\animation\BackpackAnimation;
         
         }
 
-        public function resourcepackManager(DataPacketSendEvent $event) : void
+        public function handleResourcesManager(DataPacketSendEvent $event) : void
         {
             $packets = $event->getPackets();
             
@@ -359,25 +286,12 @@ use BloomLand\Crates\animation\BackpackAnimation;
                 
                 if ($pk instanceof ResourcePackDataInfoPacket) {
 
-                    $pk->packType = ResourcePackType::RESOURCES;
                     $pk->isPremium = true;
 
                 } 
          
             }
         
-        }
-
-        public function handleManager(DataPacketReceiveEvent $event) : void
-        {
-            $pk = $event->getPacket();
-        
-            if ($pk instanceof ResourcePackClientResponsePacket) {
-
-                var_dump($pk->packIds);
-
-            }
-
         }
         
         public function handlePacketSend(DataPacketSendEvent $event) : void
@@ -419,7 +333,7 @@ use BloomLand\Crates\animation\BackpackAnimation;
 
         private function toThin(string $str) : string
         {
-            return preg_replace("/%*(([a-z0-9_]+\.)+[a-z0-9_]+)/i", "%$1", $str) . '§　';
+            return preg_replace('/%*(([a-z0-9_]+\.)+[a-z0-9_]+)/i', '%$1', $str) . '§　';
         }
 
         public function handleQuit(event\PlayerQuitEvent $event) : void
@@ -430,7 +344,7 @@ use BloomLand\Crates\animation\BackpackAnimation;
 
             AntiCheat::removeFromAntiCheats($player);
             
-			ScoreboardFactory::removeSecoreboard($player);
+            ScoreboardFactory::removeSecoreboard($player);
             
         }
 
