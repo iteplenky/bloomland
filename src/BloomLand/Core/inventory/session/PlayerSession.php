ce8291ebe96faf2ae7ad1b3c4b3c3c8931d10a79
Denis Klyuyev <42160062+iteplenky@users.noreply.github.com>
2021-08-14 18:46:50 +0600

    tabs fixed

--- src/BloomLand/Core/inventory/session/PlayerSession.php
+++ src/BloomLand/Core/inventory/session/PlayerSession.php
@@ -3,6 +3,7 @@
 
 namespace BloomLand\Core\inventory\session;
 
+
 use Closure;
 
 use BloomLand\Core\inventory\session\network\PlayerNetwork;
@@ -34,29 +35,29 @@ final class PlayerSession
      */
     public function __construct(Player $player, PlayerNetwork $network)
     {
-		$this->player = $player;
-		$this->network = $network;
-	}
+        $this->player = $player;
+        $this->network = $network;
+    }
 
-	/**
-	 * @internal
-	 */
-	public function finalize() : void
+    /**
+     * @internal
+     */
+    public function finalize() : void
     {
-		if($this->current !== null){
-			$this->current->graphic->remove($this->player);
-			$this->player->removeCurrentWindow();
-		}
-		$this->network->dropPending();
-	}
+        if($this->current !== null){
+            $this->current->graphic->remove($this->player);
+            $this->player->removeCurrentWindow();
+        }
+        $this->network->dropPending();
+    }
 
     /**
      * @return InvMenuInfo|null
      */
     public function getCurrent() : ?InvMenuInfo
     {
-		return $this->current;
-	}
+        return $this->current;
+    }
 
     /**
      * @param InvMenuInfo|null $current
@@ -64,47 +65,47 @@ final class PlayerSession
      */
     public function setCurrentMenu(?InvMenuInfo $current, ?Closure $callback = null) : void
     {
-		$this->current = $current;
+        $this->current = $current;
 
-		if($this->current !== null){
-			$this->network->waitUntil($this->network->getGraphicWaitDuration(), function(bool $success) use($callback) : void{
-				if($this->current !== null){
-					if($success && $this->current->graphic->sendInventory($this->player, $this->current->menu->getInventory())){
-						if($callback !== null){
-							$callback(true);
-						}
-						return;
-					}
+        if ($this->current !== null) {
+            $this->network->waitUntil($this->network->getGraphicWaitDuration(), function(bool $success) use($callback) : void{
+                if ($this->current !== null) {
+                    if ($success && $this->current->graphic->sendInventory($this->player, $this->current->menu->getInventory())){
+                        if ($callback !== null) {
+                            $callback(true);
+                        }
+                        return;
+                    }
 
-					$this->removeCurrentMenu();
-					if($callback !== null){
-						$callback(false);
-					}
-				}
-			});
-		}else{
-			$this->network->wait($callback ?? static function(bool $success) : void{});
-		}
-	}
+                    $this->removeCurrentMenu();
+                    if($callback !== null){
+                        $callback(false);
+                    }
+                }
+            });
+        } else {
+            $this->network->wait($callback ?? static function(bool $success) : void{});
+        }
+    }
 
     /**
      * @return PlayerNetwork
      */
     public function getNetwork() : PlayerNetwork
     {
-		return $this->network;
-	}
+        return $this->network;
+    }
 
     /**
      * @return bool
      */
     public function removeCurrentMenu() : bool
     {
-		if($this->current !== null){
-			$this->current->graphic->remove($this->player);
-			$this->setCurrentMenu(null);
-			return true;
-		}
-		return false;
-	}
+        if ($this->current !== null) {
+            $this->current->graphic->remove($this->player);
+            $this->setCurrentMenu(null);
+            return true;
+        }
+        return false;
+    }
 }
