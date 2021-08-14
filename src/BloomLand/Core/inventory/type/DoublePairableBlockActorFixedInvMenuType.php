e2e1e8108ae7838603bce9994a4ba21d3295f409
Denis Klyuyev <42160062+iteplenky@users.noreply.github.com>
2021-08-14 18:31:35 +0600

    tabs fixed

--- src/BloomLand/Core/inventory/type/DoublePairableBlockActorFixedInvMenuType.php
+++ src/BloomLand/Core/inventory/type/DoublePairableBlockActorFixedInvMenuType.php
@@ -52,19 +52,19 @@ final class DoublePairableBlockActorFixedInvMenuType implements FixedInvMenuType
      */
     public function __construct(Block $block, int $size, string $tile_id, ?InvMenuGraphicNetworkTranslator $network_translator = null)
     {
-		$this->block = $block;
-		$this->size = $size;
-		$this->tile_id = $tile_id;
-		$this->network_translator = $network_translator;
-	}
+        $this->block = $block;
+        $this->size = $size;
+        $this->tile_id = $tile_id;
+        $this->network_translator = $network_translator;
+    }
 
     /**
      * @return int
      */
     public function getSize() : int
     {
-		return $this->size;
-	}
+        return $this->size;
+    }
 
     /**
      * @param InvMenu $menu
@@ -73,35 +73,35 @@ final class DoublePairableBlockActorFixedInvMenuType implements FixedInvMenuType
      */
     public function createGraphic(InvMenu $menu, Player $player) : ?InvMenuGraphic
     {
-		$origin = $player->getPosition()->addVector(InvMenuTypeHelper::getBehindPositionOffset($player))->floor();
-		if($origin->y < World::Y_MIN || $origin->y >= World::Y_MAX){
-			return null;
-		}
+        $origin = $player->getPosition()->addVector(InvMenuTypeHelper::getBehindPositionOffset($player))->floor();
+        if($origin->y < World::Y_MIN || $origin->y >= World::Y_MAX){
+            return null;
+        }
 
-		$graphics = [];
-		$menu_name = $menu->getName();
-		foreach([
-			[$origin, $origin->east()],
-			[$origin->east(), $origin]
-		] as [$origin_pos, $pair_pos]){
-			$graphics[] = new BlockActorInvMenuGraphic(
-				$this->block,
-				$origin_pos,
-				BlockActorInvMenuGraphic::createTile($this->tile_id, $menu_name)
-					->setInt(Chest::TAG_PAIRX, $pair_pos->x)
-					->setInt(Chest::TAG_PAIRZ, $pair_pos->z),
-				$this->network_translator
-			);
-		}
+        $graphics = [];
+        $menu_name = $menu->getName();
+        foreach([
+            [$origin, $origin->east()],
+            [$origin->east(), $origin]
+        ] as [$origin_pos, $pair_pos]){
+            $graphics[] = new BlockActorInvMenuGraphic(
+                $this->block,
+                $origin_pos,
+                BlockActorInvMenuGraphic::createTile($this->tile_id, $menu_name)
+                    ->setInt(Chest::TAG_PAIRX, $pair_pos->x)
+                    ->setInt(Chest::TAG_PAIRZ, $pair_pos->z),
+                $this->network_translator
+            );
+        }
 
-		return count($graphics) > 1 ? new MultiBlockInvMenuGraphic($graphics) : $graphics[0];
-	}
+        return count($graphics) > 1 ? new MultiBlockInvMenuGraphic($graphics) : $graphics[0];
+    }
 
     /**
      * @return Inventory
      */
     public function createInventory() : Inventory
     {
-		return new InvMenuInventory($this->size);
-	}
+        return new InvMenuInventory($this->size);
+    }
 }
