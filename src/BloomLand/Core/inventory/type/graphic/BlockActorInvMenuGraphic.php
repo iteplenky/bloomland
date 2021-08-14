8fd55f4f18d65c7400c1ce220b3e30f0ae6778b3
Denis Klyuyev <42160062+iteplenky@users.noreply.github.com>
2021-08-14 18:37:40 +0600

    tabs fixed

--- src/BloomLand/Core/inventory/type/graphic/BlockActorInvMenuGraphic.php
+++ src/BloomLand/Core/inventory/type/graphic/BlockActorInvMenuGraphic.php
@@ -27,12 +27,12 @@ final class BlockActorInvMenuGraphic implements PositionedInvMenuGraphic
      */
     public static function createTile(string $tile_id, ?string $name) : CompoundTag
     {
-		$tag = CompoundTag::create()->setString(Tile::TAG_ID, $tile_id);
-		if($name !== null){
-			$tag->setString(Nameable::TAG_CUSTOM_NAME, $name);
-		}
-		return $tag;
-	}
+        $tag = CompoundTag::create()->setString(Tile::TAG_ID, $tile_id);
+        if ($name !== null) {
+            $tag->setString(Nameable::TAG_CUSTOM_NAME, $name);
+        }
+        return $tag;
+    }
 
     /**
      * @var BlockInvMenuGraphic
@@ -64,19 +64,19 @@ final class BlockActorInvMenuGraphic implements PositionedInvMenuGraphic
     #[Pure]
     public function __construct(Block $block, Vector3 $position, CompoundTag $tile, ?InvMenuGraphicNetworkTranslator $network_translator = null)
     {
-		$this->block_graphic = new BlockInvMenuGraphic($block, $position);
-		$this->position = $position;
-		$this->tile = $tile;
-		$this->network_translator = $network_translator;
-	}
+        $this->block_graphic = new BlockInvMenuGraphic($block, $position);
+        $this->position = $position;
+        $this->tile = $tile;
+        $this->network_translator = $network_translator;
+    }
 
     /**
      * @return Vector3
      */
     public function getPosition() : Vector3
     {
-		return $this->position;
-	}
+        return $this->position;
+    }
 
     /**
      * @param Player $player
@@ -84,12 +84,12 @@ final class BlockActorInvMenuGraphic implements PositionedInvMenuGraphic
      */
     public function send(Player $player, ?string $name) : void
     {
-		$this->block_graphic->send($player, $name);
-		if($name !== null){
-			$this->tile->setString(Nameable::TAG_CUSTOM_NAME, $name);
-		}
-		$player->getNetworkSession()->sendDataPacket(BlockActorDataPacket::create($this->position->x, $this->position->y, $this->position->z, new CacheableNbt($this->tile)));
-	}
+        $this->block_graphic->send($player, $name);
+        if ($name !== null) {
+            $this->tile->setString(Nameable::TAG_CUSTOM_NAME, $name);
+        }
+        $player->getNetworkSession()->sendDataPacket(BlockActorDataPacket::create($this->position->x, $this->position->y, $this->position->z, new CacheableNbt($this->tile)));
+    }
 
     /**
      * @param Player $player
@@ -98,22 +98,22 @@ final class BlockActorInvMenuGraphic implements PositionedInvMenuGraphic
      */
     public function sendInventory(Player $player, Inventory $inventory) : bool
     {
-		return $player->setCurrentWindow($inventory);
-	}
+        return $player->setCurrentWindow($inventory);
+    }
 
     /**
      * @param Player $player
      */
     public function remove(Player $player) : void
     {
-		$this->block_graphic->remove($player);
-	}
+        $this->block_graphic->remove($player);
+    }
 
     /**
      * @return InvMenuGraphicNetworkTranslator|null
      */
     public function getNetworkTranslator() : ?InvMenuGraphicNetworkTranslator
     {
-		return $this->network_translator;
-	}
+        return $this->network_translator;
+    }
 }
