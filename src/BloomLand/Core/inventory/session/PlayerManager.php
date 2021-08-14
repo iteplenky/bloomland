05751adca6861244ac19c48938d481a6b76dbd08
Denis Klyuyev <42160062+iteplenky@users.noreply.github.com>
2021-08-14 18:47:21 +0600

    tabs fixed

--- src/BloomLand/Core/inventory/session/PlayerManager.php
+++ src/BloomLand/Core/inventory/session/PlayerManager.php
@@ -37,44 +37,44 @@ final class PlayerManager
      */
     public function __construct(Plugin $registrant)
     {
-		$this->network_handler_registry = new PlayerNetworkHandlerRegistry();
+        $this->network_handler_registry = new PlayerNetworkHandlerRegistry();
 
-		$plugin_manager = Server::getInstance()->getPluginManager();
-		$plugin_manager->registerEvent(PlayerLoginEvent::class, function(PlayerLoginEvent $event) : void{
-			$this->create($event->getPlayer());
-		}, EventPriority::MONITOR, $registrant);
-		$plugin_manager->registerEvent(PlayerQuitEvent::class, function(PlayerQuitEvent $event) : void{
-			$this->destroy($event->getPlayer());
-		}, EventPriority::MONITOR, $registrant);
-	}
+        $plugin_manager = Server::getInstance()->getPluginManager();
+        $plugin_manager->registerEvent(PlayerLoginEvent::class, function(PlayerLoginEvent $event) : void{
+            $this->create($event->getPlayer());
+        }, EventPriority::MONITOR, $registrant);
+        $plugin_manager->registerEvent(PlayerQuitEvent::class, function(PlayerQuitEvent $event) : void{
+            $this->destroy($event->getPlayer());
+        }, EventPriority::MONITOR, $registrant);
+    }
 
     /**
      * @param Player $player
      */
     private function create(Player $player) : void
     {
-		static $_playerInfo = null;
-		if($_playerInfo === null){
-			$_playerInfo = new ReflectionProperty(Player::class, "playerInfo");
-			$_playerInfo->setAccessible(true);
-		}
+        static $_playerInfo = null;
+        if ($_playerInfo === null) {
+            $_playerInfo = new ReflectionProperty(Player::class, "playerInfo");
+            $_playerInfo->setAccessible(true);
+        }
 
-		$this->sessions[$player->getId()] = new PlayerSession($player, new PlayerNetwork(
-			$player->getNetworkSession(),
-			$this->network_handler_registry->get($_playerInfo->getValue($player)->getExtraData()["DeviceOS"] ?? -1)
-		));
-	}
+        $this->sessions[$player->getId()] = new PlayerSession($player, new PlayerNetwork(
+            $player->getNetworkSession(),
+            $this->network_handler_registry->get($_playerInfo->getValue($player)->getExtraData()["DeviceOS"] ?? -1)
+        ));
+    }
 
     /**
      * @param Player $player
      */
     private function destroy(Player $player) : void
     {
-		if(isset($this->sessions[$player_id = $player->getId()])){
-			$this->sessions[$player_id]->finalize();
-			unset($this->sessions[$player_id]);
-		}
-	}
+        if (isset($this->sessions[$player_id = $player->getId()])) {
+            $this->sessions[$player_id]->finalize();
+            unset($this->sessions[$player_id]);
+        }
+    }
 
     /**
      * @param Player $player
@@ -83,8 +83,8 @@ final class PlayerManager
     #[Pure]
     public function get(Player $player) : PlayerSession
     {
-		return $this->sessions[$player->getId()];
-	}
+        return $this->sessions[$player->getId()];
+    }
 
     /**
      * @param Player $player
@@ -93,14 +93,14 @@ final class PlayerManager
     #[Pure]
     public function getNullable(Player $player) : ?PlayerSession
     {
-		return $this->sessions[$player->getId()] ?? null;
-	}
+        return $this->sessions[$player->getId()] ?? null;
+    }
 
     /**
      * @return PlayerNetworkHandlerRegistry
      */
     public function getNetworkHandlerRegistry() : PlayerNetworkHandlerRegistry
     {
-		return $this->network_handler_registry;
-	}
+        return $this->network_handler_registry;
+    }
 }
