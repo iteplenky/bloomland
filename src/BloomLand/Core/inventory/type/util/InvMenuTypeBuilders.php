5a7e449bfa89697883347e3d71b8d1d6c967c45a
Denis Klyuyev <42160062+iteplenky@users.noreply.github.com>
2021-08-14 18:32:57 +0600

    tabs fixed

--- src/BloomLand/Core/inventory/type/util/InvMenuTypeBuilders.php
+++ src/BloomLand/Core/inventory/type/util/InvMenuTypeBuilders.php
@@ -16,22 +16,22 @@ final class InvMenuTypeBuilders
      */
     public static function BLOCK_ACTOR_FIXED() : BlockActorFixedInvMenuTypeBuilder
     {
-		return new BlockActorFixedInvMenuTypeBuilder();
-	}
+        return new BlockActorFixedInvMenuTypeBuilder();
+    }
 
     /**
      * @return BlockFixedInvMenuTypeBuilder
      */
     public static function BLOCK_FIXED() : BlockFixedInvMenuTypeBuilder
     {
-		return new BlockFixedInvMenuTypeBuilder();
-	}
+        return new BlockFixedInvMenuTypeBuilder();
+    }
 
     /**
      * @return DoublePairableBlockActorFixedInvMenuTypeBuilder
      */
     public static function DOUBLE_PAIRABLE_BLOCK_ACTOR_FIXED() : DoublePairableBlockActorFixedInvMenuTypeBuilder
     {
-		return new DoublePairableBlockActorFixedInvMenuTypeBuilder();
-	}
+        return new DoublePairableBlockActorFixedInvMenuTypeBuilder();
+    }
 }
