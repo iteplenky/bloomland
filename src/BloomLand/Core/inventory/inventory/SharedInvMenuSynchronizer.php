bf1944e9e47b93158da07cedd726f860422a1800
Denis Klyuyev <42160062+iteplenky@users.noreply.github.com>
2021-08-14 18:50:33 +0600

    tabs fixed

--- src/BloomLand/Core/inventory/inventory/SharedInvMenuSynchronizer.php
+++ src/BloomLand/Core/inventory/inventory/SharedInvMenuSynchronizer.php
@@ -33,20 +33,20 @@ final class SharedInvMenuSynchronizer
      */
     public function __construct(InvMenu $menu, Inventory $inventory)
     {
-		$this->inventory = $inventory;
+        $this->inventory = $inventory;
 
-		$menu_inventory = $menu->getInventory();
-		$this->synchronizer = new SharedInventorySynchronizer($menu_inventory);
-		$inventory->getListeners()->add($this->synchronizer);
+        $menu_inventory = $menu->getInventory();
+        $this->synchronizer = new SharedInventorySynchronizer($menu_inventory);
+        $inventory->getListeners()->add($this->synchronizer);
 
-		$this->notifier = new SharedInventoryNotifier($this->inventory, $this->synchronizer);
-		$menu_inventory->setContents($inventory->getContents());
-		$menu_inventory->getListeners()->add($this->notifier);
-	}
+        $this->notifier = new SharedInventoryNotifier($this->inventory, $this->synchronizer);
+        $menu_inventory->setContents($inventory->getContents());
+        $menu_inventory->getListeners()->add($this->notifier);
+    }
 
     public function destroy() : void
     {
-		$this->synchronizer->getSynchronizingInventory()->getListeners()->remove($this->notifier);
-		$this->inventory->getListeners()->remove($this->synchronizer);
-	}
+        $this->synchronizer->getSynchronizingInventory()->getListeners()->remove($this->notifier);
+        $this->inventory->getListeners()->remove($this->synchronizer);
+    }
 }
