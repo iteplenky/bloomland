966612369bca4a9c9d63ed92f1a488aba94dacb2
Denis Klyuyev <42160062+iteplenky@users.noreply.github.com>
2021-08-14 18:29:55 +0600

    tabs fixed

--- src/BloomLand/Core/inventory/InvMenuHandler.php
+++ src/BloomLand/Core/inventory/InvMenuHandler.php
@@ -35,45 +35,45 @@ final class InvMenuHandler
      */
     public static function register(Plugin $plugin) : void
     {
-		if (self::isRegistered()) {
-			throw new InvalidArgumentException($plugin->getName() . ' attempted to register ' . self::class . ' twice.');
-		}
+        if (self::isRegistered()) {
+            throw new InvalidArgumentException($plugin->getName() . ' attempted to register ' . self::class . ' twice.');
+        }
 
-		self::$registrant = $plugin;
-		self::$type_registry = new InvMenuTypeRegistry();
-		self::$player_manager = new PlayerManager(self::getRegistrant());
-		Server::getInstance()->getPluginManager()->registerEvents(new InvMenuEventHandler(self::getPlayerManager()), $plugin);
-	}
+        self::$registrant = $plugin;
+        self::$type_registry = new InvMenuTypeRegistry();
+        self::$player_manager = new PlayerManager(self::getRegistrant());
+        Server::getInstance()->getPluginManager()->registerEvents(new InvMenuEventHandler(self::getPlayerManager()), $plugin);
+    }
 
     /**
      * @return bool
      */
     public static function isRegistered() : bool
     {
-		return self::$registrant instanceof Plugin;
-	}
+        return self::$registrant instanceof Plugin;
+    }
 
     /**
      * @return Plugin
      */
     public static function getRegistrant() : Plugin
     {
-		return self::$registrant;
-	}
+        return self::$registrant;
+    }
 
     /**
      * @return InvMenuTypeRegistry
      */
     public static function getTypeRegistry() : InvMenuTypeRegistry
     {
-		return self::$type_registry;
-	}
+        return self::$type_registry;
+    }
 
     /**
      * @return PlayerManager
      */
     public static function getPlayerManager() : PlayerManager
     {
-		return self::$player_manager;
-	}
+        return self::$player_manager;
+    }
 }
