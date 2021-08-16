65e5cd83473cb3d21339455d028899cb00d66207
Denis Klyuyev <42160062+iteplenky@users.noreply.github.com>
2021-08-16 13:21:48 +0600

    VanishCommand added

---
+++ src/BloomLand/Core/command/donators/VanishCommand.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace BloomLand\Core\command\donators;
+
+
+use BloomLand\Core\command\BaseCommand;
+
+use pocketmine\player\Player;
+
+class VanishCommand extends BaseCommand
+{
+
+    /**
+     * SpyCommand constructor.
+     */
+    public function __construct()
+    {
+        parent::__construct('v', 'Спрятаться от всех игроков.', ['vanish']);
+        $this->setPermission('core.command.vanish');
+    }
+
+    /**
+     * @param Player $player
+     * @param array $args
+     */
+    public function onExecute(Player $player, array $args) : void
+    {
+        $player->setInvisible(!$player->isInvisible());
+        $player->sendMessage('Вы ' . ($player->isInvisible() ? 'включили' : 'выключили') . ' режим невидимости.');
+    }
+}
