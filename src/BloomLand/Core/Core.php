66ba03e408a8a44830baba228eaf0704dde81e0a
Denis Klyuyev <42160062+iteplenky@users.noreply.github.com>
2021-08-19 16:45:53 +0600

    Config, loggers and new rewrited code

--- src/BloomLand/Core/Core.php
+++ src/BloomLand/Core/Core.php
@@ -4,63 +4,35 @@
 namespace BloomLand\Core;
 
 
-use BloomLand\Core\controllers\Commands;
-use BloomLand\Core\controllers\Entities;
-use BloomLand\Core\controllers\Listeners;
-use BloomLand\Core\controllers\Permissions;
-use BloomLand\Core\controllers\Tasks;
+use BloomLand\Core\controllers\{
+    Commands,
+    Entities,
+    Listeners,
+    Permissions,
+    Tasks
+};
 
-use BloomLand\Core\provider\SQLite3Provider;
-use BloomLand\Core\provider\ProviderInterface;
-use BloomLand\Scoreboard\ScoreboardFactory;
+use BloomLand\Core\provider\{
+    SQLite3Provider,
+    ProviderInterface
+};
+
+use BloomLand\Core\scoreboard\ScoreboardFactory;
+
 use pocketmine\plugin\PluginBase;
 
-use pocketmine\utils\SingletonTrait;
+use pocketmine\utils\{
+    Config,
+    SingletonTrait
+};
 
 class Core extends PluginBase
 {
-
-    use SingletonTrait;
 
     /**
      * @var string
      */
     private string $prefix = ' > ';
-
-    /**
-     * @var ProviderInterface
-     */
-    private ProviderInterface $provider;
-
-    protected function onLoad() : void
-    {
-        self::setInstance($this);
-    }
-
-    protected function onEnable() : void
-    {
-        new Permissions();
-
-        $this->loadControllers();
-        $this->loadProvider();
-
-        ScoreboardFactory::init();
-    }
-
-    private function loadControllers() : void
-    {
-        $pluginManager = $this->getServer()->getPluginManager();
-
-        $pluginManager->registerEvents(new Commands(), $this);
-        $pluginManager->registerEvents(new Listeners(), $this);
-        $pluginManager->registerEvents(new Tasks(), $this);
-        $pluginManager->registerEvents(new Entities(), $this);
-    }
-
-    private function loadProvider() : void
-    {
-        $this->provider = new SQLite3Provider($this);
-    }
 
     /**
      * @return string
@@ -71,6 +43,11 @@ class Core extends PluginBase
     }
 
     /**
+     * @var ProviderInterface
+     */
+    private ProviderInterface $provider;
+
+    /**
      * @return ProviderInterface
      */
     public function getProvider() : ProviderInterface
@@ -79,11 +56,72 @@ class Core extends PluginBase
     }
 
     /**
+     * @var Config
+     */
+    private Config $config;
+
+    /**
      * @param string $key
      * @return mixed
      */
     public function get(string $key) : mixed
     {
-        return $this->getConfig()->get($key, 'Key ' . $key . ' not found.');
+        $keys = explode('.', $key);
+        if (count($keys) == 1 && $keys[0] == $key) {
+            return $this->config->get($keys[0], 'Key ' . $key . ' not found.');
+        } else {
+            $data = [];
+            foreach ($keys as $key) {
+                if (empty($data)) {
+                    $data = $this->config->get($key, 'Key ' . $key . ' not found.');
+                } else {
+                    $data = $data[$key];
+                }
+            }
+        }
+        return $data;
+    }
+
+    use SingletonTrait;
+
+    protected function onLoad() : void
+    {
+        self::setInstance($this);
+    }
+
+    protected function onEnable() : void
+    {
+        $this->config = new Config($this->getDataFolder() . 'config.yml', Config::YAML);
+
+        $this->loadControllers();
+        $this->loadConfig();
+        $this->loadProvider();
+
+        ScoreboardFactory::init();
+    }
+
+    private function loadControllers() : void
+    {
+        new Permissions();
+        new Commands();
+        new Listeners();
+        new Tasks();
+        new Entities();
+        $this->getLogger()->info('Обработчики загружены.');
+    }
+
+    private function loadConfig() : void
+    {
+        if ($this->saveDefaultConfig()) {
+            $this->getLogger()->info('Конфигурация по умолчанию загружен.');
+        }
+        $this->config = new Config($this->getDataFolder() . 'config.yml', Config::YAML);
+        $this->getLogger()->info('Конфиг сохранен.');
+    }
+
+    private function loadProvider() : void
+    {
+        $this->provider = new SQLite3Provider($this);
+        $this->getLogger()->info('База данных: ' . $this->getProvider()->getName());
     }
 }
